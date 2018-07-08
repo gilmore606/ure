5e9ac8deac0229c9da6e608d4dbc09c73619848c
gilmore <dan.burford@gmail.com>
2018-07-08 01:10:45 -0700

    fixed terrain javadoc method comments

--- src/main/java/ure/terrain/TerrainDeco.java
+++ src/main/java/ure/terrain/TerrainDeco.java
@@ -16,141 +16,159 @@ import ure.actors.UActor;
 public class TerrainDeco implements UTerrain {
 
     private UTerrain terrain;
-
+    /**
+     * Do not override the constructor.  Do your setup in becomeReal() instead.
+     */
     public TerrainDeco(UTerrain realTerrain) {
-        /**
-         * Do not override the constructor.  Do your setup in becomeReal() instead.
-         */
+
         terrain = realTerrain;
     }
-
+    /**
+     * Instantiate whatever needs to be derived from the json properties.
+     */
     public void initialize() {
-        /**
-         * Instantiate whatever needs to be derived from the json properties.
-         */
+
         terrain.initialize();
     }
-
+    /**
+     * Configure myself once I am in a real cell in the world.  This is probably where you want
+     * your setup behavior.
+     * @param c The UCell we're birthing into.  Be sure to pass this down.
+     */
     public void becomeReal(UCell c) {
-        /**
-         * Configure myself once I am in a real cell in the world.  This is probably where you want
-         * your setup behavior.
-         * @param c The UCell we're birthing into.  Be sure to pass this down.
-         */
+
         terrain.becomeReal(c);
     }
+    /**
+     * Are we passable 'by default'?
+     */
     public boolean isPassable() {
-        /**
-         * Are we passable 'by default'?
-         */
+
         return terrain.isPassable();
     }
+    /**
+     * Are we passable to this actor right now?
+     */
     public boolean isPassable(UActor actor) {
-        /**
-         * Are we passable to this actor right now?
-         */
+
         return terrain.isPassable(actor);
     }
+    /**
+     * Do we block light (regardless of who's looking)?
+     */
     public boolean isOpaque() {
-        /**
-         * Do we block light (regardless of who's looking)?
-         */
+
         return terrain.isOpaque();
     }
+    /**
+     * Does running past us stop latched auto-player-movement?
+     */
     public boolean breaksLatch() {
-        /**
-         * Does running past us stop latched auto-player-movement?
-         */
+
         return terrain.breaksLatch();
     }
-
+    /**
+     * Our 'normal' glyph.
+     */
     public char glyph() {
-        /**
-         * Our 'normal' glyph.
-         */
+
         return terrain.glyph();
     }
+    /**
+     * Our glyph at these area coordinates.  This can take PRNG variations, etc into account.
+     */
     public char glyph(int x, int y) {
-        /**
-         * Our glyph at these area coordinates.  This can take PRNG variations, etc into account.
-         */
+
         return terrain.glyph(x, y);
     }
+    /**
+     * glyph offset for realtime animation.
+     */
     public int glyphOffsetX() {
-        /**
-         * glyph offset for realtime animation.
-         */
-        return terrain.glyphOffsetX(); }
+
+        return terrain.glyphOffsetX();
+    }
+    /**
+     * glyph offset for realtime animation.
+     */
     public int glyphOffsetY() {
-        /**
-         * glyph offset for realtime animation.
-         */
+
         return terrain.glyphOffsetY();
     }
+    /**
+     * Do we glow our fgcolor into nearby tiles' lightcells?
+     */
     public boolean glow() {
-        /**
-         * Do we glow our fgcolor into nearby tiles' lightcells?
-         */
+
         return terrain.glow();
     }
+    /**
+     * How much natural sunlight can reach me?
+     */
     public float sunvis() {
-        /**
-         * How much natural sunlight can reach me?
-         */
+
         return terrain.sunvis();
     }
+    /**
+     * Do I prevent actor from moving out of me?
+     */
     public boolean preventMoveFrom(UActor actor) {
-        /**
-         * Do I prevent actor from moving out of me?
-         */
+
         return terrain.preventMoveFrom(actor);
     }
+    /**
+     * How fast can actor move into me?  (higher is faster, 1.0f is standard)
+     */
     public float moveSpeed(UActor actor) {
-        /**
-         * How fast can actor move into me?  (higher is faster, 1.0f is standard)
-         */
+
         return terrain.moveSpeed(actor);
     }
+    /**
+     * Actor attempted to walk into me (in cell).  I should either move her here, or tell her she failed.
+     */
     public void moveTriggerFrom(UActor actor, UCell cell) {
-        /**
-         * Actor attempted to walk into me (in cell).  I should either move her here, or tell her she failed.
-         */
+
         terrain.moveTriggerFrom(actor, cell);
     }
+    /**
+     * Actor walked into me (in cell).  I should print messages, apply effects, fire triggers, etc.
+     */
     public void walkedOnBy(UActor actor, UCell cell) {
-        /**
-         * Actor walked into me (in cell).  I should print messages, apply effects, fire triggers, etc.
-         */
+
         terrain.walkedOnBy(actor, cell);
     }
 
     public void printScroll(String msg, UCell cell) { terrain.printScroll(msg, cell); }
-
+    /**
+     * Update our realtime animations.
+     */
     public void animationTick() {
-        /**
-         * Update our realtime animations.
-         */
+
         terrain.animationTick();
     }
 
     public UColor bgColor() { return terrain.bgColor(); }
+    /**
+     * DNO
+     */
     public UColor bgColorBuffer() {
-        /**
-         * DNO
-         */
+
         return terrain.bgColorBuffer(); }
     public UColor fgColor() { return terrain.fgColor(); }
+    /**
+     * DNO
+     */
     public UColor fgColorBuffer() {
-        /**
-         * DNO
-         */
+
         return terrain.fgColorBuffer(); }
+    /**
+     * WARNING: URE uses the name to look up terrains internally.  Overriding this could make your system
+     * more fragile.  Think about what you're doing.
+     */
     public String name() {
-        /**
-         * WARNING: URE uses the name to look up terrains internally.  Overriding this could make your system
-         * more fragile.  Think about what you're doing.
-         */
-        return terrain.name(); }
+
+        return terrain.name();
+    }
     public String bonkmsg() { return terrain.bonkmsg(); }
 
 }
