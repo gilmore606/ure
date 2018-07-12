bb7a0e3072fb43bfc73320add32e31b121c65d61
gilmore <dan.burford@gmail.com>
2018-07-11 18:20:07 -0700

    add javadoc headings to new packages

---
+++ src/main/java/ure/commands/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Commands represent a single player-initiated unambiguous command with no parameters.
+ *
+ * Commands might cause actions, but might also simply cause UI interactions.
+ */
+
+package ure.commands;
---
+++ src/main/java/ure/dagger/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Dagger is a library which provides dependency injection.  That's what makes the 'commander' singleton available in
+ * most of URE's classes.  If you wish to make that or other global singletons available in your own code, see the
+ * notes in the dagger.AppComponent interface.
+ *
+ */
+
+package ure.dagger;
