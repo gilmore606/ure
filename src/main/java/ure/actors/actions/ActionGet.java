42c49dc78dc030b1c023e48b95855c5e8c6afd1c
gilmore <dan.burford@gmail.com>
2018-08-04 11:37:15 -0700

    only get gettable things

--- src/main/java/ure/actors/actions/ActionGet.java
+++ src/main/java/ure/actors/actions/ActionGet.java
@@ -12,14 +12,7 @@ public class ActionGet extends UAction {
 
     public static String id = "GET";
 
-    public static String nothingToGetMsg = "There's nothing here to get.";
-
     UThing thing;
-
-    public ActionGet(UActor theactor) {
-        super(theactor);
-        thing = null;
-    }
 
     public ActionGet(UActor theactor, UThing thething) {
         actor = theactor;
@@ -28,14 +21,6 @@ public class ActionGet extends UAction {
 
     @Override
     void doMe() {
-        if (thing == null) {
-            thing = actor.myCell().topThingAt();
-            if (thing == null) {
-                if (actor instanceof UPlayer) {
-                    commander.printScroll(nothingToGetMsg);
-                }
-            }
-        }
         if (!actor.tryGetThing(thing))
             suppressEvent();
     }
