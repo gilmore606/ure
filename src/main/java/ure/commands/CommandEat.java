93b2b8753af7428ba518829e1d16033a3b2c6745
gilmore <dan.burford@gmail.com>
2018-08-13 17:21:49 -0700

    genericize CommandUseVerb for making verb-specific use commands like Eat and Open

--- src/main/java/ure/commands/CommandEat.java
+++ src/main/java/ure/commands/CommandEat.java
@@ -11,36 +11,14 @@ import ure.ui.modals.UModalEntityPick;
 
 import java.util.ArrayList;
 
-public class CommandEat extends UCommand implements HearModalEntityPick {
+public class CommandEat extends CommandUseVerb {
 
     public static final String id = "EAT";
 
-    public static String nothingToEatMsg = "You don't have anything to eat.";
-    public static String eatDialog = "Eat which?";
-
-    public CommandEat() { super(id); }
-
-    @Override
-    public void execute(UPlayer player) {
-        ArrayList<Entity> edibles = new ArrayList<>();
-        for (UThing thing : player.getContents().getThings()) {
-            if (thing.useVerb().equals("eat")) {
-                edibles.add(thing);
-            }
-        }
-        if (edibles.size() < 1)
-            commander.printScroll(nothingToEatMsg);
-        else if (edibles.size() == 1) {
-            ActionUse action = new ActionUse(player, (UThing)edibles.get(0));
-            player.doAction(action);
-        } else {
-            UModalEntityPick modal = new UModalEntityPick(eatDialog, null, 0, 0, edibles, true, true, false, false, this, "eat");
-            commander.showModal(modal);
-        }
-    }
-
-    public void hearModalEntityPick(String context, Entity selection) {
-        ActionUse action = new ActionUse(commander.player(), (UThing)selection);
-        commander.player().doAction(action);
+    public CommandEat() {
+        super(id);
+        verb = "eat";
+        noTargetsMsg = "You don't have anything to eat.";
+        whichDialog = "Eat which?";
     }
 }
