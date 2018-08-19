a86e4d411c3c3817c850bdb51c6c1a3b50cad714
gilmore <dan.burford@gmail.com>
2018-08-18 23:29:06 -0700

    fix talking monk

--- src/main/java/ure/actors/actions/ActionTalk.java
+++ src/main/java/ure/actors/actions/ActionTalk.java
@@ -2,17 +2,19 @@ package ure.actors.actions;
 
 import ure.actors.UActor;
 import ure.ui.particles.ParticleTalk;
+import ure.ui.sounds.Sound;
 
 public class ActionTalk extends UAction {
 
     public static String id = "TALK";
 
     String text;
+    String[] sounds = {"sounds/talking.wav"};
 
     public ActionTalk(UActor theactor, String thetext) {
         super(theactor);
+        sound = new Sound(new String[]{"sounds/talking.wav"},0.5f);
         text = thetext;
-        sounds = new String[]{"sounds/talking.wav"};
     }
 
     @Override
