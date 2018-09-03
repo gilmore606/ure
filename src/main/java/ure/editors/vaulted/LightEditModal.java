106a1a7e469ff653348102df5349b2017008bb6f
Gilmore-laptop <dan.burford@gmail.com>
2018-09-02 19:40:47 -0700

    light flicker editing

--- src/main/java/ure/editors/vaulted/LightEditModal.java
+++ src/main/java/ure/editors/vaulted/LightEditModal.java
@@ -3,15 +3,17 @@ package ure.editors.vaulted;
 import ure.math.UColor;
 import ure.ui.ULight;
 import ure.ui.modals.UModal;
-import ure.ui.modals.widgets.Widget;
-import ure.ui.modals.widgets.WidgetButton;
-import ure.ui.modals.widgets.WidgetHSlider;
+import ure.ui.modals.widgets.*;
 
 public class LightEditModal extends UModal {
 
     VaultedModal vaulted;
     ULight light;
     WidgetHSlider rSlider,gSlider,bSlider;
+    WidgetDropdown flickerTypeWidget;
+    WidgetHSlider flickerSpeedSlider;
+    WidgetHSlider flickerIntensitySlider;
+    WidgetHSlider flickerOffsetSlider;
     WidgetButton deleteButton;
 
     public LightEditModal(VaultedModal vaulted, ULight light) {
@@ -25,17 +27,29 @@ public class LightEditModal extends UModal {
         else
             setTitle("point light");
 
-        rSlider = new WidgetHSlider(this,0,0,8,light.getColor().iR(),0,255,false);
-        gSlider = new WidgetHSlider(this,0,1,8,light.getColor().iG(),0,255,false);
-        bSlider = new WidgetHSlider(this,0,2,8,light.getColor().iB(),0,255,false);
+        rSlider = new WidgetHSlider(this,0,0,8,light.getColor().iR(),0,255,true);
+        gSlider = new WidgetHSlider(this,0,1,8,light.getColor().iG(),0,255,true);
+        bSlider = new WidgetHSlider(this,0,2,8,light.getColor().iB(),0,255,true);
         rSlider.color = new UColor(light.getColor().fR(),0f,0f);
         gSlider.color = new UColor(0f,light.getColor().fG(),0f);
         bSlider.color = new UColor(0f,0f,light.getColor().fB());
         addWidget(rSlider);
         addWidget(gSlider);
         addWidget(bSlider);
-        deleteButton = new WidgetButton(this, 0, 4, "[ Delete ]", null);
+
+        flickerTypeWidget = new WidgetDropdown(this, 5, 4, new String[]{"None", "Fire", "Pulse", "Fritz", "Blink", "Compulse"}, light.getFlickerStyle());
+        addWidget(new WidgetText(this, 0, 4, "flicker: "));
+        addWidget(flickerTypeWidget);
+        flickerSpeedSlider = new WidgetHSlider(this, 5, 5, 8, (int)(light.getFlickerSpeed()*100f), 0, 800, false);
+        addWidget(new WidgetText(this, 0, 5, "speed:"));
+        addWidget(flickerSpeedSlider);
+        flickerIntensitySlider = new WidgetHSlider(this, 5, 6, 8, (int)(light.getFlickerIntensity()*100f), 0, 100, false);
+        addWidget(new WidgetText(this,0,6,"intensity:"));
+        addWidget(flickerIntensitySlider);
+        deleteButton = new WidgetButton(this, 0, 8, "[ Delete ]", null);
         addWidget(deleteButton);
+
+        sizeToWidgets();
     }
 
     @Override
@@ -49,6 +63,8 @@ public class LightEditModal extends UModal {
         } else if (widget == bSlider) {
             bSlider.color.setB(bSlider.value);
             light.getColor().setB(bSlider.value);
+        } else if (widget == flickerTypeWidget || widget == flickerSpeedSlider || widget == flickerIntensitySlider) {
+            light.setFlicker(flickerTypeWidget.selection, (float)(flickerSpeedSlider.value)/100f, (float)(flickerIntensitySlider.value)/100f, 0);
         }
     }
 
